9b5062f51e3a00743332fa32f3dde047bb0d97dc
krongk <kenrome@gmail.com>
2012-06-11 22:38:58 +0800

    tips2

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,3 +1,14 @@
 class Dish < ActiveRecord::Base
   belongs_to :dish_cate
+
+  def photos
+    dish_dir = self.en_name.to_s.gsub(/ /, '-')
+    photo_path = "/dish/#{dish_dir}/1.jpg"
+    if File.exist?(File.join(Rails.root, 'public', photo_path))
+      %{<img src="#{photo_path}" alt="#{dish.name}"/>}
+    else
+      ""
+    end
+  end
+
 end
--- app/models/shop_dish.rb
+++ app/models/shop_dish.rb
@@ -10,16 +10,6 @@ class ShopDish < ActiveRecord::Base
   def show_dish_description
     self.dish.description
   end
-
-  def show_dish_photo
-    dish_dir = self.dish.en_name.to_s.gsub(/ /, '-')
-    photo_path = "/dish/#{dish_dir}/1.jpg"
-    if File.exist?(File.join(Rails.root, 'public', photo_path))
-      %{<img src="#{photo_path}" alt="#{dish.name}"/>}
-    else
-      ""
-    end
-  end
   
   def show_dish_price
     price.blank? ? "" : "价格：#{price}"
